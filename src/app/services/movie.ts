c811522e876d8ac37c0bce3796e32878fab1ad53
Elliodev <elliodevv@gmail.com>
2026-03-06 17:21:46 +0100

    setup movie service and secure environment

--- src/app/services/movie.ts
+++ src/app/services/movie.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Movie {
-  
+  private http = inject(HttpClient);
+
+  getTrendingMovies(): Observable<any> {
+    const url = `${environment.tmdbBaseUrl}/trending/all/week?api_key=${environment.tmdbApiKey}`;
+    return this.http.get(url);
+  }
 }
